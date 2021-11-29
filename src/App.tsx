6c0cf2b4c38134242f6ebb3cc1e62fb1aff33471
Roger9 <gutierrez.rog12@gmail.com>
2021-11-29 10:36:18 -0300

    Add repository

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyles } from './styles/Global'
+import { ContainerApp } from './styles/ContainerApp'
+
+function App () {
+  return (
+    <>
+      <GlobalStyles />
+      <ContainerApp>
+        <h1>Ecommerce App</h1>
+      </ContainerApp>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/Global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --primary: #262D39;
+    --fontColor: #FFF;
+    --colorSwitch: #262C37;
+    --colorBallSwitch: #20242b;
+    --secundary: #20262E;
+    --shadow: #C4C4C4;
+    --contrast: #9BA4B4;
+  }
+
+  [data-theme='light'] {
+    --primary: #F1F6F9;
+    --fontColor: #2C2C2C;
+    --colorSwitch: #DDE3E7;
+    --colorBallSwitch: #E5E5E6;
+    --secundary: #E4E8E9;
+    --shadow: #2C2C2C;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    position: relative;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: var(--primary);
+    color: var(--fontColor);
+    scroll-behavior: smooth;
+  }
+`
