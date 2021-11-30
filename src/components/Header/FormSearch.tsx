a89218e80b7ab50c1cb7093b9272a53351c3f268
Roger9 <gutierrez.rog12@gmail.com>
2021-11-30 18:05:53 -0300

    Component FormSearch

--- src/components/Header/FormSearch.tsx
+++ src/components/Header/FormSearch.tsx
@@ -1,9 +1,16 @@
-import { StyledFormSearch } from './style'
+import IconSearch from '../IconSearch'
+import { BoxInputSearch, StyledFormSearch } from './style'
 
 const FormSearch = () => {
   return (
     <StyledFormSearch>
-      <h1>Form</h1>
+      <input type='checkbox' name='check' id='check' />
+      <BoxInputSearch>
+        <input type='text' name='keywordSearch' id='keywordSearch' />
+      </BoxInputSearch>
+      <label htmlFor='check'>
+        <IconSearch />
+      </label>
     </StyledFormSearch>
   )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -24,5 +24,71 @@ export const StyledNav = styled.nav`
 `
 
 export const StyledFormSearch = styled.form`
-  background: red;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  input[type='checkbox'] { display:none; }
+
+  label[for='check'] {
+    height: 100%;
+    width: 2rem;
+    background: #FFF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    right: 0;
+    border-radius: 50%;
+    border: 2px solid var(--shadow);
+    cursor: pointer;
+  }
+
+  /* input[type='text'] {
+    height: 100%;
+    width: 0;
+    border-radius: 1rem;
+    border: 2px solid transparent;
+    outline: none;
+    padding: .2rem 0 .2rem 1.4rem;
+    margin-right: .2rem;
+    transition: width .2s ease-in-out;
+
+    &:focus { border: 2px solid var(--shadow) }
+  } */
+
+  input[type='text'] {
+    width: 100%;
+    height: 100%;
+    background: transparent;
+    border: none;
+    outline: none;
+    border-radius: 1rem;
+    /* padding: 0 6rem 0 1.4rem; */
+  }
+
+  input[type='text']:-webkit-autofill,input[type='text']:-webkit-autofill:focus {
+    box-shadow:0 0 0 20px #FFF inset;
+    -webkit-text-fill-color:#2C2C2C;
+    caret-color:#2C2C2C;
+  }
+
+  input[type='text']:-webkit-autofill:focus { box-shadow:0 0 0 20px #FFF inset,0 0 3px #FFF; }
+
+  input:checked ~ div { width: 24rem; }
+
+  input:checked ~ div > input { padding: 0 6rem 0 1.4rem; }
+`
+
+export const BoxInputSearch = styled.div`
+  height: 100%;
+  width: 2rem;
+  background: #FFF;
+  border-radius: 1rem;
+  border: 2px solid transparent;
+  transition: width .2s ease-in-out;
+
+  &:focus { border: 2px solid var(--shadow) }
 `
