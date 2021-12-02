78bd8cd77704e235f80809c07216873850c88be5
Roger9 <gutierrez.rog12@gmail.com>
2021-12-02 12:30:21 -0300

    Icons components created

---
+++ src/components/IconExit.tsx
@@ -0,0 +1,1 @@
+const IconExit = ({ size = 22, color = '#000000' }) => (<svg width={size} height={size} viewBox='0 0 24 24' fill='none' stroke={color} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><path d='M10 3H6a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h4M16 17l5-5-5-5M19.8 12H9' /></svg>); export default IconExit
