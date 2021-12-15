aed3ce3be2dc5943845de4cc170589d815b90928
Roger9 <gutierrez.rog12@gmail.com>
2021-12-15 18:06:49 -0300

    Path to slider created

--- src/components/Main/ContentMain/View.tsx
+++ src/components/Main/ContentMain/View.tsx
@@ -1,9 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
 import Slider from '../../Slider'
 import { StyledView } from './style'
 
 const View = () => (
   <StyledView>
-    <Slider />
+    <Routes>
+      <Route index element={<Slider />} />
+    </Routes>
   </StyledView>
 )
 
