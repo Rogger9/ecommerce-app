a84bf63471828ff3e11c4f7d5e28e2e159aca06e
Roger9 <gutierrez.rog12@gmail.com>
2021-12-19 19:15:22 -0300

    Link to SessionForm

--- src/components/Header/Nav/IconsNav.tsx
+++ src/components/Header/Nav/IconsNav.tsx
@@ -1,5 +1,5 @@
 import { Route, Link, Routes } from 'react-router-dom'
-import { $HOME, $USER } from '../../../routes'
+import { $ACCOUNTS, $HOME, $LOGIN, $USER } from '../../../routes'
 import Icon from '../../Icon'
 import IconsPathUser from './IconsPathUser'
 import { StyledIconsNav } from './style'
@@ -8,7 +8,7 @@ const IconsNav = () => (
   <StyledIconsNav>
     <Icon iconStyle='fas' icon='shopping-cart' />
     <Routes>
-      <Route index element={<Link to={$HOME + $USER}><Icon iconStyle='fas' icon='user' /></Link>} />
+      <Route index element={<Link to={$HOME + $ACCOUNTS + $LOGIN}><Icon iconStyle='fas' icon='user' /></Link>} />
       <Route path={$USER} element={<IconsPathUser />} />
     </Routes>
   </StyledIconsNav>
