13bdf6c1050a711e4c7181d74959dbfe9bf16599
Roger9 <gutierrez.rog12@gmail.com>
2021-12-20 18:34:25 -0300

    Added props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,12 @@ import { StyledButton } from '../styles/StyledButton'
 
 interface IButton {
   value: string
+  label: string
+  page: boolean
 }
 
-const Button = ({ value }: IButton) => (
-  <StyledButton>{value}</StyledButton>
+const Button = ({ value, label, page }: IButton) => (
+  <StyledButton aria-label={label} page={page}>{value}</StyledButton>
 )
 
 export default Button
