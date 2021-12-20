f6afe2c10729e829690129242bc8d95e072fd442
Roger9 <gutierrez.rog12@gmail.com>
2021-12-20 18:33:31 -0300

    Conditional render

--- src/styles/StyledButton.ts
+++ src/styles/StyledButton.ts
@@ -1,18 +1,23 @@
 import styled from 'styled-components'
 
-export const StyledButton = styled.button`
-background: #FFF;
+interface IProps {
+  page: boolean
+}
+
+export const StyledButton = styled.button<IProps>`
+  background: #FFF;
   border-radius: .3rem;
   border: 1px solid var(--shadow);
   outline: none;
-  padding: .1rem .2rem;
+  padding: ${({ page }) => page ? '.6rem' : '.1rem .2rem'};
   font-weight: bold;
   cursor: pointer;
 
-  &:active { transform: scale(.9) }
+  &:active { transform: scale(.96) }
 
   &:hover {
-    background: var(--contrast);
-    border: 1px solid var(--contrast)
+    background: ${({ page }) => page ? 'var(--primary)' : 'var(--contrast)'};
+    border: 1px solid var(--contrast);
+    color: ${({ page }) => page ? '#FFF' : undefined};
   }
 `
