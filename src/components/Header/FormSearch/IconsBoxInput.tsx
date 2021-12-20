bc6e93daaa04df9940087cbb5157fddd6ed634c5
Roger9 <gutierrez.rog12@gmail.com>
2021-12-20 18:31:30 -0300

    Button props added

--- src/components/Header/FormSearch/IconsBoxInput.tsx
+++ src/components/Header/FormSearch/IconsBoxInput.tsx
@@ -8,7 +8,7 @@ interface IShowIcons {
 const IconsBoxInput = ({ resetKeyword }: IShowIcons) => (
   <>
     <Icon iconStyle='fas' icon='times' size='1x' handleClick={resetKeyword} />
-    <Button value='Buscar' />
+    <Button label='search' value='Buscar' page={false} />
   </>
 )
 
