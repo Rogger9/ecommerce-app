939df395746e9c9ba1da85a364cc4e1a24a24852
Roger9 <gutierrez.rog12@gmail.com>
2022-01-13 23:49:01 -0300

    Functionality added to menu

--- src/components/NavBar/MenuNav/ListMenu.tsx
+++ src/components/NavBar/MenuNav/ListMenu.tsx
@@ -1,14 +1,21 @@
 import { lazy } from 'react'
 import { Link } from 'react-router-dom'
+import { $HOME } from '../../../routes'
 import { IMenuListNav } from '../../../types'
 import Icon from '../../Icon'
 import { StyledLiMenu } from './style'
 
 const SubListNav = lazy(() => import('./SubListNav'))
 
+const setPath = (path: string): string => {
+  const normalizePath = path.toLowerCase().replace(/\s/g, '-')
+  if (normalizePath === 'home') return $HOME
+  return $HOME + '/' + normalizePath
+}
+
 const ListMenu = ({ name, icon, subMenu, normalSize = true }: IMenuListNav) => (
   <StyledLiMenu>
-    <Link to='#'>
+    <Link to={setPath(name)}>
       <div><Icon iconStyle='fas' icon={icon} /></div>
       {normalSize && name}
     </Link>
