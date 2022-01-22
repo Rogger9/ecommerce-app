1250c470c8441920d20de0a41ffaa322a3eec8e1
Roger9 <gutierrez.rog12@gmail.com>
2022-01-22 19:15:09 -0300

    Simulate API

---
+++ src/api.ts
@@ -0,0 +1,18 @@
+import { booksProducts, cellphonesProducts, clothesProducts, electronicsProducts, smartTVProducts, sportsProducts, trendingProducts } from './utils/listProducts'
+import { GenericKeyString, IProducts } from './types'
+
+const listProducts: GenericKeyString<IProducts[]> = {
+  Electronics: electronicsProducts,
+  'Smart TV': smartTVProducts,
+  Cellphones: cellphonesProducts,
+  Clothes: clothesProducts,
+  Books: booksProducts,
+  Sports: sportsProducts
+}
+
+export class api {
+  static search (query?: string): Promise<IProducts[]> {
+    const result = query ? listProducts[query] : trendingProducts
+    return new Promise(resolve => setTimeout(() => resolve(result), 800))
+  }
+}
