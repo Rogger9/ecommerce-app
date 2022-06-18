882bbf46056af3a57aa43df2b854108d01ea8d44
Roger9 <gutierrez.rog12@gmail.com>
2022-06-17 23:25:56 -0300

    Config absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -6,5 +7,20 @@ export default defineConfig({
   // base: '/ecommerce/',
   build: {
     outDir: 'dist/ecommerce',
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@Entities': path.resolve(__dirname, './src/Entities'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@views': path.resolve(__dirname, './src/views'),
+    },
   },
 })
